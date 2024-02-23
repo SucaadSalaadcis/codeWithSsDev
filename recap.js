e522f8c3f6a1fa37318aa3e6be36bfd27e44cbba
sucaad salaad <SucaadSalaad520@gmail.com>
2024-02-23 07:44:06 -0800

    first commit

---
+++ recap.js
@@ -0,0 +1,40 @@
+/* <!-- html node means html element -->
+<!-- funciton resuble pices of code -->
+<!-- string represents a pice of text -->
+<!-- array represents a list of values -->
+<!-- HTML = creates visual elements on a web page --> 
+<!-- objects group different values that are related -->
+*/
+   
+   
+   /*   let todo1 = 'Get Groceires';
+            let todo2 = 'Wash Car';
+            let todo3 = 'Make Dinner';
+            let element = document.createElement("div");
+            element.innerText = todo1; */
+        // adds element to the end of the body
+        // body is the parent element wxi hoos yimaada wa children/ child
+        // it adds another child to the body 
+        // it gona put it  after all of these other child elements
+        // inside theses paraket is the element that we want to add that is the div
+        // document.body.appendChild(element);
+
+
+
+///
+
+ /* function elements (elem) {
+            let element = document.createElement("div"); 
+            element.innerText = elem;
+            document.body.appendChild(element);
+            } 
+            elements("Get Groceires")
+            // if you run the function again it will run the three line inside it
+            elements('Wash Car')
+            elements('Make Dinner')
+           */
+
+            //forEach = it goes through each value of the array
+        // takes the values of array puts the param of the function
+        // todos.push('Go to the University');
+    
